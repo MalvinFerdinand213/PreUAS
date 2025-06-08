da31271b4bef47c222eedb5ee8438da9870b3088
MalvinFerdinand213 <74392043+MalvinFerdinand213@users.noreply.github.com>
2025-06-09 03:53:05 +0700

    Update model_dev.py

--- model_dev.py
+++ model_dev.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import pickle
+
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
